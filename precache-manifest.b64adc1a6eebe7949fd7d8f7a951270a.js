cee6bf06c90f6a6d921311f5e33c7925335a32be
Amilawar <amilawarsha@gmail.com>
2022-10-19 17:46:17 +0530

    Updates

---
+++ precache-manifest.b64adc1a6eebe7949fd7d8f7a951270a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "2bdc320b6b70d0018b14",
+    "url": "/monster-hunter/static/css/main.3ba27dbd.chunk.css"
+  },
+  {
+    "revision": "2bdc320b6b70d0018b14",
+    "url": "/monster-hunter/static/js/main.744767e9.chunk.js"
+  },
+  {
+    "revision": "00f7be1914466a544ea3",
+    "url": "/monster-hunter/static/js/runtime~main.1a7efcef.js"
+  },
+  {
+    "revision": "70ce9524ae4b6666b426",
+    "url": "/monster-hunter/static/css/2.bab40a54.chunk.css"
+  },
+  {
+    "revision": "70ce9524ae4b6666b426",
+    "url": "/monster-hunter/static/js/2.094e696e.chunk.js"
+  },
+  {
+    "revision": "8fa34ba00032514f3d70",
+    "url": "/monster-hunter/static/js/3.490731cf.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/monster-hunter/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "ae9e1238d33a1a8cd2b2051129bf2a6b",
+    "url": "/monster-hunter/index.html"
+  }
+];
